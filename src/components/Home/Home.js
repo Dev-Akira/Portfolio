c291c853c8bee66f5212e27a4072cbc8fc1b00fc
Janis Flores <janisflores360@gmail.com>
2023-05-02 13:37:52 +0800

    First commited

---
+++ src/components/Home/Home.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/MyImg.jpg";
+import SocialMedia from "../SocialMedia";
+import TypeWriter from "./TypeWriter";
+
+function Home() {
+  return (
+    <section>
+      <Container fluid className="home-section" id="home">
+        <Container className="home-content">
+          <Row>
+            <Col md={6} className="home-header">
+              <h1 style={{ paddingBottom: 15 }} className="heading">
+                Hi There!{" "}
+                <span className="wave" role="img" aria-labelledby="wave">
+                  👋🏻
+                </span>
+              </h1>
+
+              <h1 className="heading-name">
+                I'M
+                <strong className="main-name"> Zhuhe Cui</strong>
+              </h1>
+
+              <p className="heading-description blockquote">
+              Dedicated and efficient full stack developer with 6+
+              years experience in application layers, presentation
+              layers, and databases. Certified in both F/E and B/E
+              technologies. Spearheaded successful transition
+              from LAMP stack to MEAN which cut latency by
+              40% and increased effectiveness of database
+              administrators by 20%. Seeking to further improve
+              HTML5 and CSS3 skills as the future full stack
+              developer at Atmospheric Solutions
+              </p>
+
+              <div className="heading-type">
+                <TypeWriter />
+              </div>
+            </Col>
+
+            <Col md={5}>
+              <img src={myImg} className="profile-pic" alt="avatar" />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+      <Container fluid className="home-about-section" id="about">
+        <Container>
+          <Row>
+            <Col md={12} className="home-about-social">
+              <h1>Get in Touch</h1>
+              <p>
+                {" "}
+                Whether you want to get in touch, or talk about a project
+                collaboration.
+                <br />
+                <strong>Feel free to connect with me</strong>
+              </p>
+              <SocialMedia />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    </section>
+  );
+}
+
+export default Home;
