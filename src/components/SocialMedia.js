884c9a0b47e592b9636a449605df9dd523d63a26
Janis Flores <janisflores360@gmail.com>
2023-05-03 10:06:42 +0800

    modified: social information

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -4,14 +4,14 @@ import {
   AiFillInstagram,
 } from "react-icons/ai";
 import { MdEmail } from "react-icons/md";
-import { FaLinkedinIn } from "react-icons/fa";
+import { FaLinkedinIn, FaSlack } from "react-icons/fa";
 
 function SocialMedia() {
   return (
     <ul className="home-about-social-links">
       <li className="social-icons">
         <a
-          href="https://github.com/sunilyadav8"
+          href="https://github.com/Hunter-janis"
           target="_blank"
           rel="noreferrer"
           className="icon-colour  home-social-icons"
@@ -21,32 +21,12 @@ function SocialMedia() {
       </li>
       <li className="social-icons">
         <a
-          href="https://www.linkedin.com/in/sunilkumaryadav84/"
-          target="_blank"
-          rel="noreferrer"
-          className="icon-colour  home-social-icons"
-        >
-          <FaLinkedinIn className="social-icon" />
-        </a>
-      </li>
-      <li className="social-icons">
-        <a
-          href="https://www.instagram.com/sunil.yadav84/"
+          href="https://www.instagram.com/Hunter-janis"
           target="_blank"
           rel="noreferrer"
           className="icon-colour home-social-icons"
         >
           <AiFillInstagram className="social-icon" />
-        </a>
-      </li>
-      <li className="social-icons">
-        <a
-          href="mailto:sunilyadav8404@gmail.com"
-          target="_blank"
-          rel="noreferrer"
-          className="icon-colour home-social-icons"
-        >
-          <MdEmail className="social-icon" />
         </a>
       </li>
     </ul>
